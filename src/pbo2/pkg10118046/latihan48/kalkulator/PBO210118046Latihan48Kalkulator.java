3cf9c764078b6b1989b48af65de3ec5e0148d8df
hilmynaufal <55622577+hilmynaufal@users.noreply.github.com>
2019-11-07 19:45:52 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118046/latihan48/kalkulator/PBO210118046Latihan48Kalkulator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10118046.latihan48.kalkulator;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : membuat program kalkulator dengan konsep pbo
+ */
+public class PBO210118046Latihan48Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kalkulator kalkulator = new Kalkulator();
+        kalkulator.setValue1(7); kalkulator.setValue2(5);
+        System.out.println("VALUE 1 = " + kalkulator.getValue1()); 
+        System.out.println("VALUE 2 = " + kalkulator.getValue2()); 
+        
+        kalkulator.setNameProject();
+        kalkulator.setNoteProject("This project shown you how to manage method in java");
+        System.out.println("Result Add is = " + kalkulator.add(kalkulator.getValue1(), kalkulator.getValue2()));
+        System.out.println("Result Minus is = " + kalkulator.minus(kalkulator.getValue1(), kalkulator.getValue2()));
+        System.out.println("Result Multiple is = " + kalkulator.multiplication(kalkulator.getValue1(), kalkulator.getValue2()));
+        System.out.println("Result Division is = " + kalkulator.division(kalkulator.getValue1(), kalkulator.getValue2()));
+    }
+
+}
